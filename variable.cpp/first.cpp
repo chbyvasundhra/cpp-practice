50b57718e547b0d1eda1b5fdad9869419e88c9a3
Vasundhra <vasundhra8@gmail.com>
2026-09-08 18:51:06 +0530

    first day practice

--- variable.cpp/first.cpp
+++ variable.cpp/first.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hello World";//Hello world
+  int age=17;//store variable
+  cout<<"my age is "<<age<<endl;//print age variable
+  int number;
+  cout<<"Enter a number:";
+  cin>>number;//take input from user
+  cout<<"you entered:"<<number<<endl;//print user input
+   cout<<"Hello World";//Hello world
+   //operators
+   int a=10,b=20;
+   cout<<"a+b="<<a+b<<endl;
+   cout<<"a-b="<<a-b<<endl;
+   cout<<"a*b="<<a*b<<endl;
+   cout<<"a/b="<<a/b<<endl;
     return 0;
 }
